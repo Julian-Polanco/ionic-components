18cce6aaa548b825bf26671a27ef293458578b84
Paula-Rojas <vpaulitarojas.09@gmail.com>
2023-11-25 01:18:43 -0500

    checks

---
+++ src/app/pages/check/check.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check',
+  templateUrl: './check.page.html',
+  styleUrls: ['./check.page.scss'],
+})
+export class CheckPage implements OnInit {
+
+  data = [
+    { name: 'primary', selected: false },
+    { name: 'secondary', selected: true },
+    { name: 'tertiary', selected: false },
+    { name: 'success', selected: true }
+  ];
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onClick(check: any) {
+    console.log(check);
+  }
+  viewData() {
+    console.log(this.data);
+  }
+}
