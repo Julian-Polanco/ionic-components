dbcfa657f234be46bbf3e2c89d91750bea134162
Paula-Rojas <vpaulitarojas.09@gmail.com>
2023-11-24 23:10:10 -0500

    action sheet component

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-action-sheet',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionSheetPage implements OnInit {
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
   }
+  onClick(){
+   this.presentActionSheet();
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Albums',
+      backdropDismiss: false,
+      buttons: [
+        {
+          icon: 'trash-outline',
+          text: 'Delete',
+          role: 'destructive',
+          cssClass: 'red',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          icon: 'share-outline',
+          text: 'Share',
+          data: {
+            action: 'share',
+          },
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          icon: 'close-outline',
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
+
 
 }
