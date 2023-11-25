b95f149102eae2a0c5c30abd6705785d99fa38eb
Paula-Rojas <vpaulitarojas.09@gmail.com>
2023-11-24 22:40:33 -0500

    base project

---
+++ src/app/pages/start/start.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+interface component{
+   icon:string;
+   name:string;
+   redirectTo:string;
+}
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './start.page.html',
+  styleUrls: ['./start.page.scss'],
+})
+export class StartPage implements OnInit {
+
+  components:component[]=[
+    {
+      icon:'american-football-outline',
+      name:'Action Sheet',
+      redirectTo:'/action-sheet'
+    },
+    {
+      icon:'logo-apple-appstore',
+      name:'Alert',
+      redirectTo:'/alert'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
