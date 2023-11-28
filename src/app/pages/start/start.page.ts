28d6890c79603f6c8c98faf3da7f57b941dd07f6
Paula-Rojas <vpaulitarojas.09@gmail.com>
2023-11-27 20:35:14 -0500

    fab and grid

--- src/app/pages/start/start.page.ts
+++ src/app/pages/start/start.page.ts
@@ -53,6 +53,11 @@ export class StartPage implements OnInit {
       icon:'car-outline',
       name:'Fab',
       redirectTo:'/fab'
+    },
+    {
+      icon:'grid-outline',
+      name:'Grid',
+      redirectTo:'/grid'
     }
   ];
 
