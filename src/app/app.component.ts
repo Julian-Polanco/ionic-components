7fe0b6055f08855dc85ee73718b665622314e47b
Paula-Rojas <vpaulitarojas.09@gmail.com>
2023-11-28 21:52:55 -0500

    menu toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { component } from 'src/app/interfaces/interfaces';
+import { DataService } from './services/data.service';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  components!: Observable<component[]>
+  
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.components = this.dataService.getMenuOptions();
+  }
 }
